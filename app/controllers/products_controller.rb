aecac803042a6e40490fd7416b811580f4c3336d
santiagomolinam <santiago.molinam@gmail.com>
2016-08-14 11:02:16 -0500

    Commit for product views creation.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,26 @@ class ProductsController < ApplicationController
     @products = Product.all
     flash[:error] = 'No products registered' if @products.empty?
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+    if @product.save
+      flash[:success] = 'product registered successfully.'
+      redirect_to products_path
+    else
+      flash[:error] = @product.errors.full_messages.join(',')
+      render 'new'
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:manufacturer_id,
+                                    :category_id, :approved,
+                                    :sended, :price)
+  end
+  private :product_params
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'products#show'
+  root to: 'products#new'
   resources :category
   resources :manufacturer
   resources :products do
