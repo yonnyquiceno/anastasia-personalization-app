3ed50a7c345d14619e8c47cbd438ee967779b1a7
santiagomolinam <santiago.molinam@gmail.com>
2016-08-14 10:25:05 -0500

    Created category, manufacturer and products views. Controllers were changed.

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,29 @@
+#:nodoc:
+class CategoryController < ApplicationController
+  def index
+    @categories = Category.all
+    flash[:error] = 'No references registered.' if @categories.empty?
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      # VehicleMailer.new_vehicle_mailer(current_user, @vehicle).deliver_now
+      flash[:success] = 'Category registered successfully.'
+      redirect_to category_index_path
+    else
+      flash[:error] = @category.errors.full_messages.join(',')
+      render 'new'
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:category_name, :description,
+                                     :image, :dimension)
+  end
+  private :category_params
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,11 @@
+#:nodoc:
 class ProductsController < ApplicationController
   def show
     @products = Product.all
+  end
+
+  def index
+    @products = Product.all
+    flash[:error] = 'No products registered' if @products.empty?
   end
 end
