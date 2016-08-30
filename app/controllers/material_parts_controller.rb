5f41d19c440a71cdffa4538139cd3eeddecce19a
Gastón Trujillo <gtrujillo@walloom.com>
2016-08-29 23:14:45 -0500

    cuasi manual rebase

--- app/controllers/material_parts_controller.rb
+++ app/controllers/material_parts_controller.rb
@@ -2,15 +2,11 @@ class MaterialPartsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    debugger
   @material = Material.find_by(color: params[:material_color])
   @material_part = MaterialPart.new(material_id: @material.id, product_id: params[:product_id], part_id: params[:part_id])
   @material_part.save
   render :nothing => true
 end
-
-private
-
 
 
 end
