4ca006cb36e8ff23779c1ce948006cf0b091aa41
Maria Paulina <je.mapau@gmail.com>
2016-08-31 18:33:37 -0500

    Purchase view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/categories/:category_id/articles/:article_id', to: 'articles#show', as: 'article_description'
 
   get '/products/cart/cart_resume', to: 'products#cart_resume', as: 'products_cart_resume'
+  get '/products/cart/cart_form', to: 'products#cart_form', as: 'cart_form'
   get '/categories/:category_id/articles/:article_id/users/:user_id/products/:product_id/parts/colour_personalization', to: 'articles#colour_personalization', as: 'article_color_personalization'
   post'/material_parts', to: 'material_parts#create'
   # get '/articles/new', to: 'articles#create', as: 'new_article'
